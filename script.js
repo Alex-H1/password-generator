b74f891b86c77df05f82826d595065f2af76f994
alex hernandez <alexanderohernandez21@gmail.com>
2022-03-31 21:30:11 -0700

    added variables and prompt

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// concated arrays
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -8,58 +12,80 @@ function writePassword() {
 
   passwordText.value = password;
 
-
 }
-
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);{
-  document.getElementById("generate").addEventListener("click",function(){
-   var length= characterlength();
-   
+generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword(){
+  var length = prompt("how many characters would you like to include in your password","8-128");
+  // makes string to numeric value
+  var passlength=parseInt(length);
+  console.log(passlength);
+  if(length < 8 || length > 128 || isNaN(length)){
+    alert("password must contain atleast 8-128 characters");
+    return;
+    
   }
-)
-
-// character length prompt
-function characterlength(){
-  var person = prompt("how many characters would you like your password to contain","8-128")
-   if (person <= 8){
-     alert("password needs to contain at least 8 characters");         
-  }
-  return person;
-}
-
-// function 
-
-function lowercase(){
-  var lowercase = confirm("would you like your password to include lowercase?");
-  return lowercase;
- 
-}
-
-function upercase(){
-  var upercase = confirm("would you like your password to include upercasses?");
-  return upercase;
-}
-
-function numeric(){
-  var numeric = confirm("would you like your password to include numbers?");
-  return numeric;
-}
-
-function specialcharacter(){
-  var specialcharacter = confirm("would you like your password ");
-  return specialcharacter;
-}
 
 
-function nothingclicked(){
-  if(lowercase !== false && upercase !== false && numeric !== false && specialcharacter !== false){
-    alert("must click one");
+var concarrays = [];
+// value arrays 
+var upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lowerCase="abcdefghijklmnopqrstuvwxyz".split("");
+var numbers="0123456789".split("");
+var specialCharacters= "+-*/|\]}[{]!@#$%^&()`~".split("");
+
+
+if(upperCasecon !== false && lowerCasecon !==false && numberscon !== false && specialCharacterscon !==false){
+  alert("must click atleast on of the criteria");
+} 
+
+
+// prompt for lowercases
+var lowerCasecon = confirm("would you like your password to contain lowercases?");
+  if(lowerCasecon === true){
+      for(var i =0; i <lowerCase.length; i++) {
+      concarrays.push(lowerCase[i]);
   }
 }
 
+// prompt for uppercase
 
+var upperCasecon = confirm("would you like your password to contain upercases?");
+  if(upperCasecon === true){
+    for(var i=0; i<upperCase.length; i++){
+      concarrays.push(upperCase[i]);
+    }
+}
 
+var numberscon = confirm("do you want your password to contain numeric?");
+  if(numberscon === true){
+    for(var i=0; i<upperCase.length;i++){
+      concarrays.push(upperCase[i]);
+      }
+    } 
 
+var specialCharacterscon = confirm("would you like your password to contain spacial characters?");
+  if (specialCharacterscon === true){
+    for(var i=0; i<specialCharacters.length;i++){
+      concarrays.push(specialCharacters[i]);
+    }
+  }
 
+ var passwords ="";
+ for(var i = 0; i < passlength; i++){
+  concarrays[
+    Math.floor(Math.random()*concarrays.length)
+  ];
+  passwords += concarrays[
+    Math.floor(Math.random()*concarrays.length)
+  ];
+  
+  }
+  console.log(passwords);
+  return passwords;
+  
+  
+}
+    
