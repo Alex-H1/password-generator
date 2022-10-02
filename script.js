aa7c74a48c6bd439538d9544884d9d4368e7f876
alex-h <alexanderohernandez21@gmail.com>
2022-10-01 19:59:28 -0700

    start refactor

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("generate");
 var passwordEl  = document.getElementById("password");
-// concated arrays
 
 
 
@@ -9,9 +8,7 @@ var passwordEl  = document.getElementById("password");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  passwordEl.value = password;
 
 }
 // Add event listener to generate button
@@ -26,51 +23,51 @@ function generatePassword(){
     alert("password must contain atleast 8-128 characters");
     return;
     
-  }
+  };
 
 
-var concarrays = [];
-// value arrays 
-var upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-var lowerCase="abcdefghijklmnopqrstuvwxyz".split("");
-var specialCharacters= "+-*/|\]}[{]!@#$%^&()`~".split("");
-var numeric ="1234567890".split("")
+  var concarrays = [];
+  // value arrays 
+  var upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  var lowerCase="abcdefghijklmnopqrstuvwxyz".split("");
+  var specialCharacters= "+-*/|\]}[{]!@#$%^&()`~".split("");
+  var numeric ="1234567890".split("");
 
 
-// prompt for lowercases
-var lowerCasecon = confirm("would you like your password to contain lowercases?");
+  // prompt for lowercases
+  var lowerCasecon = confirm("would you like your password to contain lowercases?");
   if(lowerCasecon === true){
       for(var i =0; i <lowerCase.length; i++) {
       concarrays.push(lowerCase[i]);
-  }
-}
+    };
+  };
 
-// prompt for uppercase
+  // prompt for uppercase
 
-var upperCasecon = confirm("would you like your password to contain upercases?");
+  var upperCasecon = confirm("would you like your password to contain upercases?");
   if(upperCasecon === true){
     for(var i=0; i<upperCase.length; i++){
       concarrays.push(upperCase[i]);
-    }
-}
+    };
+  };
 
-var numberscon = confirm("do you want your password to contain numeric?");
+  var numberscon = confirm("do you want your password to contain numeric?");
   if(numberscon === true){
     for(var i=0; i<numeric.length;i++){
       concarrays.push(numeric[i]);
-      }
-    } 
+      };
+    };
 
-var specialCharacterscon = confirm("would you like your password to contain spacial characters?")
+  var specialCharacterscon = confirm("would you like your password to contain spacial characters?")
   if (specialCharacterscon === true){
     for(var i=0; i<specialCharacters.length;i++){
       concarrays.push(specialCharacters[i]);
-    }
-  }
+    };
+  };
 
   if(lowerCasecon === false && upperCasecon === false && specialCharacterscon === false && numberscon === false){
     alert("must click atleast one of the criteria");
-  } 
+  };
   
 
  var passwords ="";
@@ -81,8 +78,7 @@ var specialCharacterscon = confirm("would you like your password to contain spac
   passwords += concarrays[
     Math.floor(Math.random()*concarrays.length)
   ];
-  
-  }
+  };
   console.log(passwords);
   
   return passwords;
